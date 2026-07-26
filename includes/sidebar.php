9c4e934129009188d46320c12040da0ede6112fc
mohammedasawer41-cpu <mohammedasawer41@gmail.com>
2026-07-26 10:48:55 +0100

    Fix: Update navigation links to work from any directory level using dynamic base path

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,5 +1,11 @@
+<?php
+// Determine the base URL
+$pathDepth = substr_count($_SERVER['PHP_SELF'], '/') - 2;
+$basePath = str_repeat('../', $pathDepth);
+if($pathDepth < 0) $basePath = '';
+?>
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-<a href="dashboard.php" class="brand-link">
+<a href="<?= $basePath ?>dashboard.php" class="brand-link">
 <span class="brand-text">
 SafeTrack HSE
 </span>
@@ -8,43 +14,43 @@ SafeTrack HSE
 <nav>
 <ul class="nav nav-pills nav-sidebar flex-column">
 <li class="nav-item">
-<a href="dashboard.php" class="nav-link">
+<a href="<?= $basePath ?>dashboard.php" class="nav-link">
 <i class="nav-icon fas fa-home"></i>
 <p>Dashboard</p>
 </a>
 </li>
 <li class="nav-item">
-<a href="modules/planning/monthly.php" class="nav-link">
+<a href="<?= $basePath ?>modules/planning/monthly.php" class="nav-link">
 <i class="nav-icon fas fa-calendar-alt"></i>
 <p>Inspection Planning</p>
 </a>
 </li>
 <li class="nav-item">
-<a href="modules/inspections/" class="nav-link">
+<a href="<?= $basePath ?>modules/inspections/index.php" class="nav-link">
 <i class="nav-icon fas fa-clipboard-check"></i>
 <p>Inspections</p>
 </a>
 </li>
 <li class="nav-item">
-<a href="modules/anomalies/" class="nav-link">
+<a href="<?= $basePath ?>modules/anomalies/index.php" class="nav-link">
 <i class="nav-icon fas fa-exclamation-triangle"></i>
 <p>Anomalies</p>
 </a>
 </li>
 <li class="nav-item">
-<a href="modules/actions/" class="nav-link">
+<a href="<?= $basePath ?>modules/actions/index.php" class="nav-link">
 <i class="nav-icon fas fa-tasks"></i>
 <p>CAPA</p>
 </a>
 </li>
 <li class="nav-item">
-<a href="modules/reports/" class="nav-link">
+<a href="<?= $basePath ?>modules/reports/index.php" class="nav-link">
 <i class="nav-icon fas fa-chart-bar"></i>
 <p>Reports</p>
 </a>
 </li>
 <li class="nav-item">
-<a href="modules/users/" class="nav-link">
+<a href="<?= $basePath ?>modules/users/index.php" class="nav-link">
 <i class="nav-icon fas fa-users"></i>
 <p>Users</p>
 </a>
