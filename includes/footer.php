2642f49a1a7a00c4e4164a7f97284bc127efc365
mohammedasawer41-cpu <mohammedasawer41@gmail.com>
2026-07-26 10:49:15 +0100

    Fix: Update script paths to work from any directory level

--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,16 @@
+<?php
+// Determine the base URL for scripts
+$pathDepth = substr_count($_SERVER['PHP_SELF'], '/') - 2;
+$basePath = str_repeat('../', $pathDepth);
+if($pathDepth < 0) $basePath = '';
+?>
 <footer class="main-footer">
 <strong>SafeTrack HSE V3</strong>
 </footer>
 </div>
-<script src="assets/adminlte/plugins/jquery/jquery.min.js"></script>
-<script src="assets/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="assets/adminlte/dist/js/adminlte.min.js"></script>
+<script src="<?= $basePath ?>assets/adminlte/plugins/jquery/jquery.min.js"></script>
+<script src="<?= $basePath ?>assets/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?= $basePath ?>assets/adminlte/dist/js/adminlte.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </body>
 </html>
