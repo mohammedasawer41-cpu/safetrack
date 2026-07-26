a690d44d8ef533433475f7c58027899c95717015
mohammedasawer41-cpu <mohammedasawer41@gmail.com>
2026-07-26 10:49:07 +0100

    Fix: Update asset paths to work from any directory level

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,10 @@
 <?php
 require_once __DIR__ . '/../config/database.php';
-$basePath = __DIR__;
+
+// Determine the base URL dynamically based on the current file location
+$pathDepth = substr_count($_SERVER['PHP_SELF'], '/') - 2; // -2 for /safetrack and the file
+$basePath = str_repeat('../', $pathDepth);
+if($pathDepth < 0) $basePath = '';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,9 +12,9 @@ $basePath = __DIR__;
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width,initial-scale=1">
 <title>SafeTrack HSE</title>
-<link rel="stylesheet" href="assets/adminlte/plugins/fontawesome-free/css/all.min.css">
-<link rel="stylesheet" href="assets/adminlte/dist/css/adminlte.min.css">
-<link rel="stylesheet" href="assets/css/style.css">
+<link rel="stylesheet" href="<?= $basePath ?>assets/adminlte/plugins/fontawesome-free/css/all.min.css">
+<link rel="stylesheet" href="<?= $basePath ?>assets/adminlte/dist/css/adminlte.min.css">
+<link rel="stylesheet" href="<?= $basePath ?>assets/css/style.css">
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
